3997eb5f6ae6fe6125a39f1927909e8d6f1d36f5
Deuteriunt. <1619167551@qq.com>
2024-07-18 18:11:17 +0800

    Update Danheng-Proxy

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+
+namespace Danheng.Proxy
+{
+    internal static class Program
+    {
+        private const string Title = "Danheng Proxy (Alter)";
+        private const string ConfigPath = "config.json";
+        private const string ConfigTemplatePath = "config.tmpl.json";
+
+        private static ProxyService s_proxyService = null!;
+        private static bool s_clearupd = false;
+        
+        private static void Main(string[] args)
+        {
+            Console.Title = Title;
+            Console.WriteLine($"Danheng.Proxy");
+            CheckProxy();
+            InitConfig();
+
+            var conf = JsonSerializer.Deserialize(File.ReadAllText(ConfigPath), ProxyConfigContext.Default.ProxyConfig) ?? throw new FileLoadException("Please correctly configure config.json.");
+            s_proxyService = new ProxyService(conf.DestinationHost, conf.DestinationPort, conf);
+            Console.WriteLine($"代理正在运行");
+            AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+            Console.CancelKeyPress += OnProcessExit;
+
+            Thread.Sleep(-1);
+        }
+
+
+
+        private static void InitConfig()
+        {
+            if (!File.Exists(ConfigPath))
+            {
+                File.Copy(ConfigTemplatePath, ConfigPath);
+            }
+        }
+
+        private static void OnProcessExit(object? sender, EventArgs? args)
+        {
+            if (s_clearupd) return;
+            s_proxyService?.Shutdown();
+            s_clearupd = true;
+        }
+
+        public static void CheckProxy()
+        {
+            try
+            {
+                string? ProxyInfo = GetProxyInfo();
+                if (ProxyInfo != null)
+                {
+                    Console.WriteLine("嗯...看来你正在使用其他代理软件(例如 Clash、V2RayN、Fiddler 等)");
+                    Console.WriteLine($"您的系统代理: {ProxyInfo}");
+                    Console.WriteLine("您必须关闭所有其他代理软件以确保 Danheng.Proxy 能够正常工作.");
+                    Console.WriteLine("如果您关闭了其他代理软件或者您认为没有使用其他代理，请按任意键继续.");
+                    Console.ReadKey();
+                }
+            }
+            catch (NullReferenceException)
+            {
+
+            }
+        }
+
+        public static string? GetProxyInfo()
+        {
+            try
+            {
+                IWebProxy proxy = WebRequest.GetSystemWebProxy();
+                Uri? proxyUri = proxy.GetProxy(new Uri("https://www.example.com"));
+                if (proxyUri == null) return null;
+
+                string proxyIP = proxyUri.Host;
+                int proxyPort = proxyUri.Port;
+                string info = proxyIP + ":" + proxyPort;
+                return info;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Danheng-Proxy
- 
+# Danheng.Proxy
+
+A simple Proxy for playing Private Servers.
+
+## Usage
+
+Go to [releases page](https://github.com/Deuteriunt/Danheng.Proxy/releases) for the latest pre-built binary. It don't require any .NET versions.
